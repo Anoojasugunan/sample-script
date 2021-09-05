7fee6182c1062102e2c1040f2d35d6838e77ee35
Anoojasugunan <anooja.sugunan@vismayacorp.com>
2021-09-05 21:24:03 +0530

    first app file committing

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.get ('/',(req,res) => {
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.gets ('/about', (req,res) => {
+    res.sendFile(path.join(__dirname+'/about.html'));
+  });
+
+app.listen( 8081, () => {
+    console.log('Listening on port 8081');
+});
